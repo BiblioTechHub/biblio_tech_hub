abd36e5b7b86d665981bd04457363c38c8c6bb22
Antonio Diaz <antoniodp0502@gmail.com>
2023-11-11 13:23:54 +0100

    main configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:biblio_tech_hub/config/router/app_router.dart';
 import 'package:biblio_tech_hub/config/theme/app_theme.dart';
+import 'package:biblio_tech_hub/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Asegurar que Flutter esté inicializado
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
