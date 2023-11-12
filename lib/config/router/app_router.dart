e6dd60513a8a65a93e2fc4ae5c4556e661a48a1e
Antonio Diaz <antoniodp0502@gmail.com>
2023-11-12 13:39:53 +0100

    add screens

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,12 +1,29 @@
 
 import 'package:biblio_tech_hub/presentation/screens/home_screen.dart';
+import 'package:biblio_tech_hub/presentation/screens/loan_screen.dart';
+import 'package:biblio_tech_hub/presentation/screens/profile_screen.dart';
+import 'package:biblio_tech_hub/presentation/screens/search_screen.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => HomeScreen(),
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: '/search',
+          builder: (context, state) => const SearchScreen(),
+        ),
+        GoRoute(
+          path: '/loan',
+          builder: (context, state) => const LoanScreen(),
+        ),
+        GoRoute(
+          path: '/profile',
+          builder: (context, state) => const ProfileScreen(),
+        ),
+      ]
     )
   ]
 );
---
+++ lib/presentation/screens/loan_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoanScreen extends StatelessWidget {
+  const LoanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('LoanScreen'),
+      ),
+    );
+  }
+}
