d365b65051fbc21c372e47b4b3cd9cd1a7e6e462
Antonio Diaz <antoniodp0502@gmail.com>
2023-11-17 20:52:54 +0100

    HorizontalListViewImplemented

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:biblio_tech_hub/domain/entities/book.dart';
 import 'package:biblio_tech_hub/presentation/riverpod/book_stock_provider.dart';
 import 'package:biblio_tech_hub/presentation/widgets/logo_and_title.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,7 @@ class HomeView extends StatelessWidget {
             const LogoAndTitle(),
             SizedBox(height: size.height * 0.05),
 
-            _HorizontalListView(size: size),
+            HorizontalListView(size: size),
           ]
         ),
       ),
@@ -29,39 +30,84 @@ class HomeView extends StatelessWidget {
   }
 }
 
-class _HorizontalListView extends ConsumerWidget{
-  const _HorizontalListView({
+class HorizontalListView extends ConsumerStatefulWidget{
+  const HorizontalListView({super.key, 
     required this.size,
   });
 
   final Size size;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HorizontalListView> createState() => _HorizontalListViewState();
+}
+
+class _HorizontalListViewState extends ConsumerState<HorizontalListView> {
+
+  @override
+  void initState() {
+    super.initState();
     ref.watch(bookStockProvider.notifier).getBook();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final books = ref.watch(bookStockProvider);
-    return Expanded(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: size.width * 0.05, vertical: size.height * 0.02),
-        decoration: const BoxDecoration(
-          border: Border.symmetric(horizontal: BorderSide(width: 1)), 
-          color: Colors.white
-        ),
-        alignment: Alignment.centerLeft,
-        width: size.width,
-        child: Column(
-          children: [
-            Text('LO MAS NUEVO >', style: TextStyle(fontFamily: 'Bangers', fontSize: size.height * 0.04)),
-            Expanded(
-              child: ListView.builder(
-                itemCount: books.length,
-                itemBuilder: (context, index) {
-                  return Text(books[index].title);
-                }
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: widget.size.width * 0.05, vertical: widget.size.height * 0.02),
+      decoration: const BoxDecoration(
+        border: Border.symmetric(horizontal: BorderSide(width: 1)), 
+        color: Colors.white
+      ),
+      alignment: Alignment.centerLeft,
+      width: widget.size.width,
+      height: widget.size.height * 0.5,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('LO MAS NUEVO >', style: TextStyle(fontFamily: 'Bangers', fontSize: widget.size.height * 0.04)),
+          Expanded(child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            physics: const BouncingScrollPhysics(),
+            itemCount: books.length,
+            itemBuilder: (context, index) {
+              return _Slide(book: books[index], size: widget.size,);
+            },
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  const _Slide({required this.book, required this.size});
+
+  final Book book;
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(size.width * 0.03),
+      child: Column(
+        children: [
+          SizedBox(
+            width: size.width * 0.4,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: FadeInImage(
+                height: size.height * 0.3,
+                fit: BoxFit.fill,
+                placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+                image: NetworkImage(book.imageLinks['smallThumbnail'])
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          SizedBox(
+            width: size.width * 0.4,
+            child: Text(book.title, style: const TextStyle(fontWeight: FontWeight.bold), maxLines: 2)),
+          // SizedBox(width: size.width * 0.3)
+        ],
       ),
     );
   }
