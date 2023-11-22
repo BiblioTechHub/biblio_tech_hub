3217f40ae36679694b4fd8adbb22edf43e6bd426
Carlos Rivero <carlos10.arco@gmail.com>
2023-11-22 08:36:56 +0100

    searcch view added

--- lib/domain/repositories/book_repository.dart
+++ lib/domain/repositories/book_repository.dart
@@ -3,5 +3,7 @@ import 'package:biblio_tech_hub/domain/entities/book.dart';
 abstract class BookRepository {
 
   Future<Book> getBookByISBN(String isbn);
+
+  Future<Book> getBookByTitle(String title);
   
 }
