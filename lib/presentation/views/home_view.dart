6913cc92000aeddea37732b289f4a35f005fb731
Antonio Diaz <91115968+i02dipea@users.noreply.github.com>
2023-11-23 14:47:31 +0100

    Update home_view.dart

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -3,7 +3,6 @@ import 'package:biblio_tech_hub/presentation/riverpod/book_details_view_provider
 import 'package:biblio_tech_hub/presentation/riverpod/book_stock_provider.dart';
 import 'package:biblio_tech_hub/presentation/widgets/logo_and_title.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
